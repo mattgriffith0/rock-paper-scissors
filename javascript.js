4b4e8178cdef925246f46f6be92293676878142a
Matt Griffith <mattgriffith0@gmail.com>
2020-01-26 17:18:10 -0800

    function and CSS refactorig with Bill.

--- javascript.js
+++ javascript.js
@@ -5,13 +5,13 @@ let playerScore = 0;
 let round = 0;
 
 const rock = document.querySelector("#rock");
-rock.addEventListener('click', chooseRock);
+rock.addEventListener('click', choice);
 
 const paper = document.querySelector("#paper");
-paper.addEventListener('click', choosePaper);
+paper.addEventListener('click', choice);
 
 const scissors = document.querySelector("#scissors");
-scissors.addEventListener('click', chooseScissors);
+scissors.addEventListener('click', choice);
 
 function restartButton() {
     const restart = document.createElement('button');
@@ -38,7 +38,7 @@ function computerChoice() {
     }
 }
 
-function insertRound() {
+function insertRound(victor) {
   var scoretable = document.getElementById("table");
   var row = scoretable.insertRow(-1);
   var cell = row.insertCell(0);
@@ -50,88 +50,33 @@ function insertRound() {
   cell.innerHTML = round;
   cell2.innerHTML = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
   cell3.innerHTML = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-  cell4.innerHTML = "Tie";
+  cell4.innerHTML = victor;
   cell5.innerHTML = playerScore;
   cell6.innerHTML = compScore;
-}
-
-function insertRoundWin() {
-    var scoretable = document.getElementById("table");
-    var row = scoretable.insertRow(-1);
-    var cell = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    cell.innerHTML = round;
-    cell2.innerHTML = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    cell3.innerHTML = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-    cell4.innerHTML = winner;
-    cell5.innerHTML = playerScore;
-    cell6.innerHTML = compScore;
+  if (victor === "Player") {
     cell5.setAttribute('class', 'win');
-}
-
-function insertRoundLose() {
-  var scoretable = document.getElementById("table");
-  var row = scoretable.insertRow(-1);
-  var cell = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
-  var cell6 = row.insertCell(5);
-  cell.innerHTML = round;
-  cell2.innerHTML = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-  cell3.innerHTML = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-  cell4.innerHTML = winner;
-  cell5.innerHTML = playerScore;
-  cell6.innerHTML = compScore;
-  cell6.setAttribute('class', 'win');
+  } else if (victor === "Computer") {
+    cell6.setAttribute('class', 'win');
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
+  const winMatrix = {   rock: 'scissors',   paper: 'rock',   scissors: 'paper' }
   if (playerSelection === computerSelection) {
     round++;
-    insertRound();
+    insertRound("Tie");
     return "It's a Tie!"
-  } else if (computerSelection === "rock" && playerSelection === "paper" ) {
-    winner = "Player";
+  } else {
     round++;
-    playerScore ++;
-    insertRoundWin();
-    return `You win - paper beats rock! The score is now Player: ${playerScore} Computer: ${compScore}`;
-  } else if (computerSelection === "rock" && playerSelection === "scissors" ) {
-    winner = "Computer";
-    round++;
-    compScore ++;
-    insertRoundLose();
-    return `You lose - rock beats scissors! The score is now Player: ${playerScore} Computer: ${compScore}`;
-  } else if (computerSelection === "paper" && playerSelection === "rock" ) {
-    winner = "Computer";
-    round++;
-    compScore ++;
-    insertRoundLose();
-    return `You lose - paper beats rock! The score is now Player: ${playerScore} Computer: ${compScore}`;
-  } else if (computerSelection === "paper" && playerSelection === "scissors" ) {
-    winner = "Player"
-    round++;
-    playerScore ++;
-    insertRoundWin();
-    return `You win - scissors beats paper! The score is now Player: ${playerScore} Computer: ${compScore}`;
-  } else if (computerSelection === "scissors" && playerSelection === "paper" ) {
-    winner = "Computer";
-    round++;
-    compScore ++;
-    insertRoundLose();
-    return `You lose - scissors beats paper! The score is now Player: ${playerScore} Computer: ${compScore}`;
-  } else if (computerSelection === "scissors" && playerSelection === "rock" ) {
-    winner = "Player";
-    round++;
-    playerScore ++;
-    insertRoundWin();
-    return `You win - rock beats scissors! The score is now Player: ${playerScore} Computer: ${compScore}`;
+    if (winMatrix[playerSelection] === computerSelection) {
+      playerScore++;
+      insertRound("Player");
+      return `You win - ${playerSelection} beats ${computerSelection}! The score is now Player: ${playerScore} Computer: ${compScore}`;
+    } else {
+      compScore++;
+      insertRound("Computer");
+      return `You lose - ${computerSelection} beats ${playerSelection}! The score is now Player: ${playerScore} Computer: ${compScore}`;
+    }
   }
 }
 
@@ -157,26 +102,8 @@ function finished() {
   }
 }
 
-function chooseRock() {
-  playerSelection = 'rock';
-  computerSelection = computerChoice();
-  document.getElementById("playerchoice").innerHTML = `You've chosen ${playerSelection}.`;
-  document.getElementById("compchoice").innerHTML = `The computer chose ${computerSelection}.`;
-  playRound(playerSelection, computerSelection);
-  finished();
-}
-
-function choosePaper() {
-  playerSelection = 'paper';
-  computerSelection = computerChoice();
-  document.getElementById("playerchoice").innerHTML = `You've chosen ${playerSelection}.`;
-  document.getElementById("compchoice").innerHTML = `The computer chose ${computerSelection}.`;
-  playRound(playerSelection, computerSelection);
-  finished();
-}
-
-function chooseScissors() {
-  playerSelection = 'scissors';
+function choice(click) {
+  playerSelection = click.target.id;
   computerSelection = computerChoice();
   document.getElementById("playerchoice").innerHTML = `You've chosen ${playerSelection}.`;
   document.getElementById("compchoice").innerHTML = `The computer chose ${computerSelection}.`;
